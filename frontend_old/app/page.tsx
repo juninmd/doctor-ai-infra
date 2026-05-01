fba3760fb11efbd2e85df8548b00220c7faf9c35
Antonio Junior <jr_acn@hotmail.com>
2026-05-01 09:59:06 -0300

    fix: removed

--- frontend_old/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from "@/components/chat";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100 p-4">
-      <Chat />
-    </main>
-  );
-}
